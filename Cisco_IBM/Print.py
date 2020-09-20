c4f4f2acf0d254d495f80a4a15d0c5f47239dfeb
dacugo <dcg1410@hotmail.com>
2020-09-20 09:41:43 -0500

    Función print curso Cisco

---
+++ Cisco_IBM/Print.py
@@ -0,0 +1,14 @@
+#La función 'print' puede aceptar casi cualquier tipo de recurso o variable y mostrarlo en pantalla
+print("¡Hola, Mundo!")
+
+#En el segundo módulo del programa se muestra su uso con un dato vacío para dar un salto de línea
+print("La Witsi Witsi Araña subió su telaraña.")
+print()
+print("Vino la lluvia y se la llevó.")
+
+#Sin embargo los saltos de línea se pueden realizar dentro de los parametros de la función 'print'
+#Esto se realiza con el carácter de escape '\'
+#esta intrucción le dice al sistema que la siguente carácter que se ingrese se lea como una instrucción
+#para el caso del salto de línea se emplea '\n', n que proviene de 'newline'
+print("\nLa Witsi Witsi Araña\nsubió su telaraña.\n")
+print("Vino la lluvia\ny se la llevó.")
---
+++ Linkedin learning/TipoVariables.py
@@ -0,0 +1,15 @@
+#Numero
+edad = 21
+
+#Numero con decimal
+Estatura = 175.7
+
+#Cadena
+Nombre = "Carlos"
+
+#Booleano
+Verdadero = True
+Falso = False
+
+#Numero complejo
+NC = 7 + 8j
